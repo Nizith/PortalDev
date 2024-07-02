91c648959a959e0390eaafa61005adf8dae1ceaa
Nizith <nisithalakshan94@gmail.com>
2024-07-02 13:37:10 +0530

    connected to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = 4500;
+app.listen( PORT, () => { console.log(`App is running on port : ${PORT}`)});
+
+const URL = "mongodb+srv://nisithalakshan94:portaldevSLT@slt.ffxoqv6.mongodb.net/PortalDev"
+
+mongoose.connect( URL )
+.then( () => {
+    console.log("MongoDB Connection Successfull!");
+})
+.catch( (err) => {
+    console.log("MongoDB Connection UNsuccessfull!", err);
+})
+
+
+
