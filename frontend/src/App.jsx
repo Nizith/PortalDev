0cec30590f25d36fbb165681d85b7fd45ff379a5
Nizith <nisithalakshan94@gmail.com>
2024-07-17 09:07:25 +0530

    urls added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,7 @@ import AdminLogin from "./Components/Admin/AdminLogin.jsx"
 import Sectionregistration from "./Components/Admin/SectionAdd.jsx"
 import Supplier from "./Components/Admin/SupplierAdd.jsx"
 import CustomerAdd from "./Components/Admin/CustomerAdd.jsx"
-<<<<<<< HEAD
 import ContractAdd from "./ContractAdd.jsx"
-=======
-import PaymentAdd from "./Components/PaymentAdd.jsx"
->>>>>>> 795aa99ea6bd1e5e6ec45dbd8267469ef3d030e5
 
 function App() {
   return (
@@ -20,21 +16,12 @@ function App() {
       <Routes>
 
         <Route path="/" element={ <AdminLogin /> } />
-        <Route path="/admindash" element={ < Bodycomponent /> } />  
-<<<<<<< HEAD
+        <Route path="/admindash" element={ < Bodycomponent /> } />
         <Route path="/addsection" element = { <Sectionregistration />} />
         <Route path="/addsupplier" element={<Supplier/>}/>
         <Route path="/addcustomer" element = {<CustomerAdd/>} />
         <Route path="/addcontract" element = {<ContractAdd/>} />
         
-=======
-        <Route path="/sectionreg" element = { <Sectionregistration />} />
-        <Route path="/supplierreg" element={<Supplier/>}/>
-        <Route path="/createcustomer" element = {<CustomerAdd/>} />
-        <Route path="/addpayment" element = {<PaymentAdd/>}/>
-
-      
->>>>>>> 795aa99ea6bd1e5e6ec45dbd8267469ef3d030e5
       </Routes>
     </div>
       
