d2344dfce7d1360f9c646fa1e2a7b666cbf2fdef
D-il <163960309+D-il@users.noreply.github.com>
2024-09-26 12:32:15 +0530

    Updated PaymentAdd , Cretaed Payment components.

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -6,7 +6,7 @@ router.post('/portaldev/createpayment',paymentCTRL.createPayment);
 router.get('/portaldev/Allpayments',paymentCTRL.getAllPayment);
 router.get('/portaldev/payments/:id',paymentCTRL.getOnePayment);
 router.put('/portaldev/updatepayments/:id',paymentCTRL.updatePayment);
-router.delete('portaldev/deletepayment/:id',paymentCTRL.deletePayment);
+router.delete('/portaldev/deletepayment/:id',paymentCTRL.deletePayment);
 
 
 module.exports = router;
